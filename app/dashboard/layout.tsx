b48fe063a60ef0c74de10e2b2dbd345162a03759
kaushik-24 <grg23kaushik@gmail.com>
2025-02-11 12:39:28 +0545

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 // app/dashboard/layout.tsx
-import { getSession } from '@auth0/nextjs-auth0';
 import DashboardGrid from './(components)/DashBoardGrid';
 
 export default async function DashboardLayout({
@@ -7,8 +6,6 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getSession();
-
   return (
     <div className="">
       <DashboardGrid>
